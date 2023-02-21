b522ffee6bea109c452114a527761d8ca57b9cc6
farmhutsoftwareteam <108745208+farmhutsoftwareteam@users.noreply.github.com>
2023-02-21 12:53:28 +0200

    jbk

--- components/Dashboard/dashlayout.jsx
+++ components/Dashboard/dashlayout.jsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import React, { useState } from 'react';
 import Cookies from 'js-cookie';
 import {useRouter} from 'next/router';
+import ThemeToggler from '../Header/ThemeToggler';
 
 const handleLogout = async (req, res) => {
     Cookies.remove('token');
@@ -22,16 +23,7 @@ const Dashlayout = () => {
             </div>
 
             <div className="px-4 mt-8">
-                <label for="" className="sr-only"> Search </label>
-                <div className="relative">
-                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                        <svg className="w-5 h-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                        </svg>
-                    </div>
-
-                    <input type="search" name="" id="" className="block w-full py-2 pl-10 border border-gray-300 rounded-lg focus:ring-indigo-600 focus:border-indigo-600 sm:text-sm" placeholder="Search here" />
-                </div>
+               <ThemeToggler  color="inherit"/>
             </div>
 
             <div className="px-4 mt-6">
