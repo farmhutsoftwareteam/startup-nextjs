491995f9f27c851adcaf2542fce59347014e6f7e
farmhutsoftwareteam <108745208+farmhutsoftwareteam@users.noreply.github.com>
2023-02-21 14:38:02 +0200

    favicon

--- app/head.jsx
+++ app/head.jsx
@@ -4,7 +4,7 @@ export default function Head() {
       <title>Farmhut - Digital Tools to make Agriculture More Efficient</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Revolutininzg the future of the Agriculture Food Chain" />
-      <link rel="icon" href="https://res.cloudinary.com/vambo/image/upload/v1675953341/mundalogo_2_sdm9lb.png" />
+      <link rel="icon" href="https://res.cloudinary.com/vambo/image/upload/v1676983039/Untitled_design_74_ysba5u.png" />
     </>
   );
 }
