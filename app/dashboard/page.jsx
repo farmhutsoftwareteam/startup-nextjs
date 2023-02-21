c6ea7a76e1667922ca38f52c5eab656cda0d3851
farmhutsoftwareteam <108745208+farmhutsoftwareteam@users.noreply.github.com>
2023-02-21 13:26:00 +0200

    changed dashboard layout

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,20 +1,15 @@
 import Breadcrumb from "@/components/Common/Breadcrumb";
 import Dashlayout from "@/components/Dashboard/dashlayout";
+import DashNav from "@/components/Dashboard/dashnav";
 import PageShell from "@/components/Dashboard/pageShell";
 
 
 const Dashboard = () => {
   return (
     <>
-  
-  <div class="flex">
-      <div class="w-1/6 bg-gray-300">
-        <Dashlayout />
-      </div>
-      <div class="w-5/6 bg-gray-200">
+    <DashNav />
         <PageShell />
-      </div>
-    </div>
+   
    
   
 
---
+++ components/Dashboard/dashnav.jsx
@@ -0,0 +1,32 @@
+"use client"
+import React from "react";
+import { Navbar, Button, Link, Text, Card, Radio, Image } from "@nextui-org/react";
+
+export default function DashNav() {
+
+    
+
+    return(
+        <Navbar isBordered  variant="sticky">
+            <Navbar.Brand>
+                <Navbar.Toggle aria-label="toggle navigation" />
+                    
+            </Navbar.Brand>
+            <Navbar.Content enableCursorHighlight hideIn="xs" variant="underline">
+                <Navbar.Link href='#'>Dashboard</Navbar.Link>
+                <Navbar.Link href='#'>Profile</Navbar.Link>
+                <Navbar.Link href='#'>Settings</Navbar.Link>
+                <Navbar.Link href='#'>Help</Navbar.Link>
+            </Navbar.Content>
+            <Navbar.Collapse>
+                <Navbar.CollapseItem>Dashboard</Navbar.CollapseItem>
+                <Navbar.CollapseItem>Profile</Navbar.CollapseItem>
+                <Navbar.CollapseItem>Settings</Navbar.CollapseItem>
+                <Navbar.CollapseItem>Help</Navbar.CollapseItem>
+
+            </Navbar.Collapse>
+
+
+        </Navbar>
+    )
+}
